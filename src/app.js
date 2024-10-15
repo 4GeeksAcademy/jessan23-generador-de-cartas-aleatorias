a82adde2cca4b1b12d44fbbbf5e6f932175a74c6
Jessan23 <jessicaanairayagrez@gmail.com>
2024-10-15 05:36:21 +0000

    falta poner styles

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generatecard() {
+  // Arreglos con los palos y los valores de la carta
+  const suits = ["♦", "♥", "♠", "♣"];
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "K",
+    "Q"
+  ];
+
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+
+  // Inserta los valores generados en los elementos del DOM
+  document.getElementsByClassName("top-suit")[0].textContent = randomSuit;
+  document.getElementsByClassName("card-number")[0].textContent = randomValue;
+  document.getElementsByClassName("bottom-suit")[0].textContent = randomSuit;
+
+  // Cambia el color de la carta dependiendo del palo
+  const cardElement = document.getElementsByClassName("card")[0]; // Accede al primer elemento con clase "card"
+  if (randomSuit === "♥" || randomSuit === "♦") {
+    cardElement.style.color = "red";
+  } else {
+    cardElement.style.color = "black";
+  }
+}
+
+// Llamada a la función para generar la carta al cargar la página
+window.onload = generatecard;
